ad67ff8717f21dc9f5665db157d4884774c13fcb
Marco Pivetta <ocramius@gmail.com>
2014-04-18 15:47:16 +0200

    Adding service definitions for the `ApplicationInspectorListenerFactory`

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,14 @@
 <?php
 
-return [];
+namespace Roave\DeveloperTools;
+
+use Roave\DeveloperTools\Mvc\Factory\ApplicationInspectorListenerFactory;
+use Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener;
+
+return [
+    'service_manager' => [
+        'factories' => [
+            ApplicationInspectorListener::class => ApplicationInspectorListenerFactory::class,
+        ],
+    ],
+];
