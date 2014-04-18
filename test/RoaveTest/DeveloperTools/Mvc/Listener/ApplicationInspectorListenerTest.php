a9d587317bc4f97663f99d1a8cb794ba53d50b8a
Marco Pivetta <ocramius@gmail.com>
2014-04-18 13:51:49 +0200

    Implementing tests about the possible logic workflow about an application inspector listener

---
+++ test/RoaveTest/DeveloperTools/Mvc/Listener/ApplicationInspectorListenerTest.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace RoaveTest\DeveloperTools\Inspector;
+
+use PHPUnit_Framework_TestCase;
+use Roave\DeveloperTools\Inspection\InspectionInterface;
+use Roave\DeveloperTools\Inspector\InspectorInterface;
+use Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener;
+use Roave\DeveloperTools\Repository\InspectionRepositoryInterface;
+use Roave\DeveloperTools\Repository\UUIDGenerator\UUIDGeneratorInterface;
+use Zend\EventManager\EventInterface;
+
+/**
+ * Tests for {@see \Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener}
+ *
+ * @covers \Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener
+ */
+class ApplicationInspectorListenerTest extends PHPUnit_Framework_TestCase
+{
+    public function testListenerTriggering()
+    {
+        $repository    = $this->getMock(InspectionRepositoryInterface::class);
+        $event         = $this->getMock(EventInterface::class);
+        $inspection    = $this->getMock(InspectionInterface::class);
+        $inspector     = $this->getMock(InspectorInterface::class);
+        $uuidGenerator = $this->getMock(UUIDGeneratorInterface::class);
+        $inspectionId  = '123456';
+
+        $listener = new ApplicationInspectorListener($repository, $uuidGenerator, $inspector);
+
+        $inspector->expects($this->any())->method('inspect')->with($event)->will($this->returnValue($inspection));
+        $repository->expects($this->atLeastOnce())->method('add')->with($inspectionId, $inspection);
+
+        $this->assertSame('123456', $listener->collectInspectionsOnApplicationFinish($event));
+    }
+}
