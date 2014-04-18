7836bf8ec4dcb78296102d1469ef13cd5b2dbbbe
Marco Pivetta <ocramius@gmail.com>
2014-04-18 09:05:40 +0200

    Implementing base aggregate inspection

---
+++ src/Roave/DeveloperTools/Inspection/AggregateInspection.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace Roave\DeveloperTools\Inspection;
+
+/**
+ * An inspection that contains multiple other inspections
+ */
+class AggregateInspection implements InspectionInterface
+{
+    /**
+     * @var InspectionInterface[]
+     */
+    private $inspections;
+
+    /**
+     * @param InspectionInterface[] $inspections
+     */
+    public function __construct($inspections) {
+        $this->inspections = $inspections;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function serialize()
+    {
+        return serialize($this->inspections);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function unserialize($serialized)
+    {
+        $this->inspections = unserialize($serialized);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInspectionData()
+    {
+        return $this->inspections;
+    }
+}
