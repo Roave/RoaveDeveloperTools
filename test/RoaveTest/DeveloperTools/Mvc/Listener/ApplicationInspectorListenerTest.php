780a01b9d45eb2115d6b34941805835d58ea8953
Marco Pivetta <ocramius@gmail.com>
2014-04-18 14:19:17 +0200

    Adding tests to verify aggregate listener logic of the `ApplicationInspectorListener`

--- test/RoaveTest/DeveloperTools/Mvc/Listener/ApplicationInspectorListenerTest.php
+++ test/RoaveTest/DeveloperTools/Mvc/Listener/ApplicationInspectorListenerTest.php
@@ -25,6 +25,7 @@ use Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener;
 use Roave\DeveloperTools\Repository\InspectionRepositoryInterface;
 use Roave\DeveloperTools\Repository\UUIDGenerator\UUIDGeneratorInterface;
 use Zend\EventManager\EventInterface;
+use Zend\EventManager\EventManagerInterface;
 
 /**
  * Tests for {@see \Roave\DeveloperTools\Mvc\Listener\ApplicationInspectorListener}
@@ -40,13 +41,31 @@ class ApplicationInspectorListenerTest extends PHPUnit_Framework_TestCase
         $inspection    = $this->getMock(InspectionInterface::class);
         $inspector     = $this->getMock(InspectorInterface::class);
         $uuidGenerator = $this->getMock(UUIDGeneratorInterface::class);
-        $inspectionId  = '123456';
 
         $listener = new ApplicationInspectorListener($repository, $uuidGenerator, $inspector);
 
+        $uuidGenerator->expects($this->any())->method('generateUUID')->will($this->returnValue('123456'));
         $inspector->expects($this->any())->method('inspect')->with($event)->will($this->returnValue($inspection));
-        $repository->expects($this->atLeastOnce())->method('add')->with($inspectionId, $inspection);
+        $repository->expects($this->atLeastOnce())->method('add')->with('123456', $inspection);
 
         $this->assertSame('123456', $listener->collectInspectionsOnApplicationFinish($event));
+    }
+
+    public function testAttach()
+    {
+        $eventManager  = $this->getMock(EventManagerInterface::class);
+        $repository    = $this->getMock(InspectionRepositoryInterface::class);
+        $inspector     = $this->getMock(InspectorInterface::class);
+        $uuidGenerator = $this->getMock(UUIDGeneratorInterface::class);
+
+        $listener = new ApplicationInspectorListener($repository, $uuidGenerator, $inspector);
+
+        $eventManager->expects($this->once())->method('attach')->with(
+            $this->isType('string'),
+            $this->isType('callable'),
+            $this->isType('integer')
+        );
+
+        $listener->attach($eventManager);
     }
 }
