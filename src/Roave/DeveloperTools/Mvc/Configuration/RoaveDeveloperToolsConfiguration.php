74d764e478faeed98ac61ab72c420902d5287538
Marco Pivetta <ocramius@gmail.com>
2014-04-19 11:30:21 +0200

    Adding RoaveDeveloperTools configuration object

---
+++ src/Roave/DeveloperTools/Mvc/Configuration/RoaveDeveloperToolsConfiguration.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace Roave\DeveloperTools\Mvc\Configuration;
+
+use Zend\EventManager\ListenerAggregateTrait;
+use Zend\Stdlib\AbstractOptions;
+
+/**
+ * RoaveDeveloperTools configuration
+ */
+class RoaveDeveloperToolsConfiguration extends AbstractOptions
+{
+    /**
+     * @var string[] names of the inspector services
+     *               (pointing to instances of {@see \Roave\DeveloperTools\Inspector\InspectorInterface})
+     */
+    private $inspectors = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($options)
+    {
+        $this->__strictMode__ = true;
+
+        parent::__construct($options);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getInspectors()
+    {
+        return $this->inspectors;
+    }
+
+    /**
+     * @param string[] $inspectors
+     */
+    public function setInspectors(array $inspectors)
+    {
+        $this->inspectors = array_map(
+            function ($inspector) {
+                return (string) $inspector;
+            },
+            $inspectors
+        );
+    }
+}
