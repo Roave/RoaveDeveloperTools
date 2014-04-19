b2b7a74923728b0da0a908b6aa6fc6848e80ffb7
Marco Pivetta <ocramius@gmail.com>
2014-04-19 21:04:54 +0200

    Implementing a `ListInspections` controller

---
+++ test/RoaveTest/DeveloperTools/Mvc/Controller/ListInspectionsControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace RoaveTest\DeveloperTools\Mvc\Inspection;
+
+use PHPUnit_Framework_TestCase;
+use Roave\DeveloperTools\Inspection\InspectionInterface;
+use Roave\DeveloperTools\Mvc\Configuration\RoaveDeveloperToolsConfiguration;
+use Roave\DeveloperTools\Mvc\Controller\ListInspectionsController;
+use Roave\DeveloperTools\Renderer\InspectionRendererInterface;
+use Roave\DeveloperTools\Repository\InspectionRepositoryInterface;
+use Zend\Stdlib\Exception\BadMethodCallException;
+use Zend\Stdlib\RequestInterface;
+use Zend\View\Model\ModelInterface;
+
+/**
+ * Tests for {@see \Roave\DeveloperTools\Mvc\Controller\ListInspectionsController}
+ *
+ * @covers \Roave\DeveloperTools\Mvc\Controller\ListInspectionsController
+ */
+class ListInspectionsControllerTest extends PHPUnit_Framework_TestCase
+{
+    public function testDispatch()
+    {
+        $repository  = $this->getMock(InspectionRepositoryInterface::class);
+        $renderer    = $this->getMock(InspectionRendererInterface::class);
+        $inspection1 = $this->getMock(InspectionInterface::class);
+        $inspection2 = $this->getMock(InspectionInterface::class);
+
+        $controller = new ListInspectionsController($repository, $renderer);
+
+        $repository->expects($this->any())->method('getAll')->will($this->returnValue([$inspection1, $inspection2]));
+        $renderer
+            ->expects($this->any())
+            ->method('canRender')
+            ->with($this->logicalOr($inspection1, $inspection2))
+            ->will($this->returnValue(true));
+        $renderer
+            ->expects($this->any())
+            ->method('render')
+            ->with($this->logicalOr($inspection1, $inspection2))
+            ->will($this->returnValue($this->getMock(ModelInterface::class)));
+
+        $result = $controller->dispatch($this->getMock(RequestInterface::class));
+
+        $this->assertInstanceOf(ModelInterface::class, $result);
+        $this->assertSame([$inspection1, $inspection2], $result->getVariable('inspections'));
+        $this->assertCount(2, $result->getVariable('inspectionModels'));
+        $this->assertInstanceOf(ModelInterface::class, $result->getVariable('inspectionModels')[0]);
+        $this->assertInstanceOf(ModelInterface::class, $result->getVariable('inspectionModels')[1]);
+    }
+}
