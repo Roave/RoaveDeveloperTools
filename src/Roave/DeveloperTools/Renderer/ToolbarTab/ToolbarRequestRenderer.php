ed594000dbd1db0579871218f506320a21ae0441
Marco Pivetta <ocramius@gmail.com>
2014-04-21 12:16:27 +0200

    Refactoring request renderer

--- src/Roave/DeveloperTools/Renderer/ToolbarTab/ToolbarRequestRenderer.php
+++ src/Roave/DeveloperTools/Renderer/ToolbarTab/ToolbarRequestRenderer.php
@@ -18,33 +18,21 @@
 
 namespace Roave\DeveloperTools\Renderer\ToolbarTab;
 
-use Roave\DeveloperTools\Inspection\InspectionInterface;
 use Roave\DeveloperTools\Mvc\Inspection\RequestInspection;
-use Roave\DeveloperTools\Renderer\InspectionRendererInterface;
-use Zend\View\Model\ViewModel;
+use Roave\DeveloperTools\Renderer\BaseInspectionRenderer;
 
 /**
  * Renders request inspections' data
  */
-class ToolbarRequestRenderer implements InspectionRendererInterface
+class ToolbarRequestRenderer extends BaseInspectionRenderer
 {
     /**
      * {@inheritDoc}
      */
-    public function canRender(InspectionInterface $inspection)
-    {
-        return $inspection instanceof RequestInspection;
-    }
+    protected $supportedInspection = RequestInspection::class;
 
     /**
      * {@inheritDoc}
      */
-    public function render(InspectionInterface $inspection)
-    {
-        $viewModel = new ViewModel(['inspection' => $inspection]);
-
-        $viewModel->setTemplate('roave-developer-tools/toolbar/tab/request');
-
-        return $viewModel;
-    }
+    protected $templateName = 'roave-developer-tools/toolbar/tab/request';
 }
