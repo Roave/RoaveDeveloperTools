1251ffee416ce789c4b46365b6fbd7c74aa3711f
Marco Pivetta <ocramius@gmail.com>
2014-04-21 11:09:53 +0200

    Specification for a base renderer implementation

---
+++ test/RoaveTest/DeveloperTools/Renderer/BaseRendererTest.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace RoaveTest\DeveloperTools\Renderer;
+
+use PHPUnit_Framework_TestCase;
+use Roave\DeveloperTools\Inspection\AggregateInspection;
+use Roave\DeveloperTools\Inspection\InspectionInterface;
+use Roave\DeveloperTools\Inspection\TimeInspection;
+use Roave\DeveloperTools\Renderer\BaseRenderer;
+use Zend\View\Model\ModelInterface;
+
+/**
+ * Tests for {@see \Roave\DeveloperTools\Renderer\BaseRenderer}
+ *
+ * @covers \Roave\DeveloperTools\Renderer\BaseRenderer
+ */
+class BaseRendererTest extends PHPUnit_Framework_TestCase
+{
+    public function testAcceptsOnlyAggregateInspection()
+    {
+        /* @var $renderer BaseRenderer */
+        $renderer = $this->getMockForAbstractClass(BaseRenderer::class);
+
+        $this->assertTrue($renderer->canRender($this->getMock(InspectionInterface::class)));
+        $this->assertTrue($renderer->canRender($this->getMock(TimeInspection::class, [], [], '', false)));
+        $this->assertTrue($renderer->canRender($this->getMock(AggregateInspection::class, [], [], '', false)));
+    }
+
+    public function testRender()
+    {
+        /* @var $renderer BaseRenderer */
+        $renderer = $this->getMockForAbstractClass(BaseRenderer::class);
+
+        /* @var $inspection InspectionInterface */
+        $inspection = $this->getMock(InspectionInterface::class);
+        $viewModel  = $renderer->render($inspection);
+
+        $this->assertInstanceOf(ModelInterface::class, $viewModel);
+
+        $this->assertSame($inspection, $viewModel->getVariable('inspection'));
+    }
+}
