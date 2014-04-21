e758ca012c1b8e4a5889fd85f79514f684b84015
Marco Pivetta <ocramius@gmail.com>
2014-04-21 11:43:03 +0200

    Removing code duplication from the toolbar renderer tests

--- test/RoaveTest/DeveloperTools/Renderer/ToolbarInspectionRendererTest.php
+++ test/RoaveTest/DeveloperTools/Renderer/ToolbarInspectionRendererTest.php
@@ -18,97 +18,20 @@
 
 namespace RoaveTest\DeveloperTools\Renderer;
 
-use PHPUnit_Framework_TestCase;
-use Roave\DeveloperTools\Inspection\AggregateInspection;
-use Roave\DeveloperTools\Inspection\InspectionInterface;
-use Roave\DeveloperTools\Inspection\TimeInspection;
-use Roave\DeveloperTools\Renderer\BaseAggregateInspectionRenderer;
-use Roave\DeveloperTools\Renderer\InspectionRendererInterface;
 use Roave\DeveloperTools\Renderer\ToolbarInspectionRenderer;
-use Zend\View\Model\ModelInterface;
 
 /**
  * Tests for {@see \Roave\DeveloperTools\Renderer\ToolbarInspectionRenderer}
  *
  * @covers \Roave\DeveloperTools\Renderer\ToolbarInspectionRenderer
  */
-class ToolbarInspectionRendererTest extends PHPUnit_Framework_TestCase
+class ToolbarInspectionInspectionRendererTest extends BaseAggregateInspectionRendererTest
 {
-    public function testAcceptsOnlyAggregateInspection()
+    /**
+     * {@inheritDoc}
+     */
+    public function getRenderer(array $detailRenderers)
     {
-        $renderer = new ToolbarInspectionRenderer([]);
-
-        $this->assertFalse($renderer->canRender($this->getMock(InspectionInterface::class)));
-        $this->assertFalse($renderer->canRender($this->getMock(TimeInspection::class, [], [], '', false)));
-        $this->assertTrue($renderer->canRender($this->getMock(AggregateInspection::class, [], [], '', false)));
-    }
-
-    public function testRendersWithEmptyTabRenderers()
-    {
-        $renderer   = new ToolbarInspectionRenderer([]);
-        $inspection = new AggregateInspection([]);
-        $viewModel  = $renderer->render($inspection);
-
-        $this->assertInstanceOf(ModelInterface::class, $viewModel);
-
-        $this->assertSame($inspection, $viewModel->getVariable('inspection'));
-        $this->assertSame([], $viewModel->getVariable(BaseAggregateInspectionRenderer::PARAM_DETAIL_MODELS));
-    }
-
-    public function testRendersWithSingleTabRenderer()
-    {
-        $tabRenderer        = $this->getMock(InspectionRendererInterface::class);
-        $wrappedInspection1 = $this->getMock(InspectionInterface::class);
-        $wrappedInspection2 = $this->getMock(InspectionInterface::class);
-        $wrappedModel       = $this->getMock(ModelInterface::class);
-        $renderer           = new ToolbarInspectionRenderer([$tabRenderer]);
-        $inspection         = new AggregateInspection([$wrappedInspection1, $wrappedInspection2]);
-
-        $tabRenderer
-            ->expects($this->exactly(2))
-            ->method('canRender')
-            ->with($this->logicalOr($wrappedInspection1, $wrappedInspection2))
-            ->will($this->returnValue(true));
-
-        $tabRenderer
-            ->expects($this->exactly(2))
-            ->method('render')
-            ->with($this->logicalOr($wrappedInspection1, $wrappedInspection2))
-            ->will($this->returnValue($wrappedModel));
-
-        $viewModel = $renderer->render($inspection);
-
-        $this->assertCount(2, $viewModel->getChildren());
-
-        $this->assertSame(
-            [[$wrappedModel, $wrappedModel]],
-            $viewModel->getVariable(BaseAggregateInspectionRenderer::PARAM_DETAIL_MODELS)
-        );
-    }
-
-    public function testRendersWithMultipleTabRenderers()
-    {
-        $tabRenderer1       = $this->getMock(InspectionRendererInterface::class);
-        $tabRenderer2       = $this->getMock(InspectionRendererInterface::class);
-        $wrappedInspection1 = $this->getMock(InspectionInterface::class);
-        $wrappedInspection2 = $this->getMock(InspectionInterface::class);
-        $wrappedModel       = $this->getMock(ModelInterface::class);
-        $renderer           = new ToolbarInspectionRenderer([$tabRenderer1, $tabRenderer2]);
-        $inspection         = new AggregateInspection([$wrappedInspection1, $wrappedInspection2]);
-
-        $tabRenderer1->expects($this->any())->method('canRender')->will($this->returnValue(true));
-        $tabRenderer2->expects($this->any())->method('canRender')->will($this->returnValue(true));
-
-        $tabRenderer1->expects($this->any())->method('render')->will($this->returnValue($wrappedModel));
-        $tabRenderer2->expects($this->any())->method('render')->will($this->returnValue($wrappedModel));
-
-        $viewModel = $renderer->render($inspection);
-
-        $this->assertCount(4, $viewModel->getChildren());
-
-        $this->assertSame(
-            [[$wrappedModel, $wrappedModel], [$wrappedModel, $wrappedModel]],
-            $viewModel->getVariable(BaseAggregateInspectionRenderer::PARAM_DETAIL_MODELS)
-        );
+        return new ToolbarInspectionRenderer($detailRenderers);
     }
 }
