51e72ee0d2993a66fc4fed46cc77dd3821620dd1
Marco Pivetta <ocramius@gmail.com>
2014-05-04 06:44:16 +0200

    Explicit service location

--- admin/app/scripts/controller/listInspectionsCtrl.js
+++ admin/app/scripts/controller/listInspectionsCtrl.js
@@ -2,10 +2,10 @@
 
 angular
     .module('adminApp')
-    .controller('listInspectionsCtrl', function ($scope, $inspectionsRepository) {
+    .controller('listInspectionsCtrl', ['$scope', '$inspectionsRepository', function ($scope, $inspectionsRepository) {
         $scope.inspectionIds = [];
 
         $inspectionsRepository.getInspectionIds().then(function (data) {
             $scope.inspectionIds = data;
         });
-    });
+    }]);
--- admin/app/scripts/controller/main.js
+++ admin/app/scripts/controller/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('adminApp')
-    .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', ['$scope', function ($scope) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-    });
+    }]);
