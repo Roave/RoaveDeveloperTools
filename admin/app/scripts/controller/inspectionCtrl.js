ae8a768bfdd4aaa03957c2da3a114c81205a567a
Marco Pivetta <ocramius@gmail.com>
2014-05-14 03:43:39 +0200

    `inspectionCtrl` should set the current reports map in the scope

--- admin/app/scripts/controller/inspectionCtrl.js
+++ admin/app/scripts/controller/inspectionCtrl.js
@@ -2,10 +2,15 @@
 
 angular
     .module('RoaveDeveloperToolsAdmin')
-    .controller('inspectionCtrl', ['$scope', '$routeParams', '$inspectionsRepository', function ($scope, $routeParams, $inspectionsRepository) {
+    .controller(
+        'inspectionCtrl',
+        ['$scope', '$routeParams', '$inspectionsRepository', 'RDT_REPORTS', function ($scope, $routeParams, $inspectionsRepository, RDT_REPORTS) {
         $scope.inspection = null;
 
         $inspectionsRepository.getInspectionById($routeParams.inspectionId).then(function (inspection) {
             $scope.inspection = inspection;
         });
+
+        // @todo should be an object, not just an array
+        $scope.reports = RDT_REPORTS;
     }]);
