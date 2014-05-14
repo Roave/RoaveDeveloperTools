2c5a1a7be803ce6a843a37d1e0b2c78a5cb1c390
Marco Pivetta <ocramius@gmail.com>
2014-05-14 04:22:41 +0200

    `InspectionCtrl` loads sub-inspections in the scope

--- admin/app/scripts/controller/inspectionCtrl.js
+++ admin/app/scripts/controller/inspectionCtrl.js
@@ -5,10 +5,12 @@ angular
     .controller(
         'inspectionCtrl',
         ['$scope', '$routeParams', '$inspectionsRepository', 'RDT_REPORTS', function ($scope, $routeParams, $inspectionsRepository, RDT_REPORTS) {
-        $scope.inspection = null;
+        $scope.inspection     = null;
+        $scope.subInspections = null;
 
         $inspectionsRepository.getInspectionById($routeParams.inspectionId).then(function (inspection) {
-            $scope.inspection = inspection;
+            $scope.inspection     = inspection;
+            $scope.subInspections = inspection.getSubInspections();
         });
 
         // @todo should be an object, not just an array
