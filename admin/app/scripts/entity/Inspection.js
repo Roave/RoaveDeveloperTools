854c6eeabe4c9eddf171b0193730bdb312e14b0e
Marco Pivetta <ocramius@gmail.com>
2014-05-14 04:13:58 +0200

    Inspection should be able to render child items

--- admin/app/scripts/entity/Inspection.js
+++ admin/app/scripts/entity/Inspection.js
@@ -6,5 +6,29 @@
         this.data = data;
     }
 
+    Inspection.prototype = {
+        getType: function () {
+            return this.data.inspectionType;
+        },
+
+        getSubInspections: function () {
+            var inspectionData = this.data.inspectionData,
+                idx = 0,
+                id = this.id;
+
+            if (!Array.isArray(inspectionData)) {
+                return [];
+            }
+
+            return inspectionData.map(function (inspection) {
+                return new Inspection(id + '-' + (idx += 1), inspection);
+            });
+        },
+
+        getData: function () {
+            return this.data.inspectionData;
+        }
+    };
+
     exports.Inspection = Inspection;
 }(window));
